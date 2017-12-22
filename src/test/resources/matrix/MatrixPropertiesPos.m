e6a67efd3573ca405568f58da139e4003f0624e9
PhilippGoe <philipp.goerick@rwth-aachen.de>
2017-12-22 16:55:11 +0100

    Added tests

--- src/test/resources/matrix/MatrixPropertiesPos.m
+++ src/test/resources/matrix/MatrixPropertiesPos.m
@@ -31,15 +31,16 @@ norm Q^{3,3} a4 = [1 1 0;0 1 1;1 0 1];
 square Q^{3,3} a5 = [9 -3 7;5 2 -6;-3/4 2 6];
 diag Q^{3,3} a6 = [0 0 0;0 0 0;0 0 0];
 psd Q^{3,3} a7 = [2 -1,0;-1 2 -1;0 -1 2];
+nsd Q^{3,3} a8 = [-2 1 0;1 -2 1;0 1 -2];
 
 //right operations
 herm Q^{3,3} c1 = a1 + a1;
 herm Q^{3,3} c2 = a1 - a1;
-//herm Q^{3,3} c3 = a1 * 2;
+herm Q^{3,3} c3 = 2.5 * a1;
 herm Q^{3,3} c4 = a1^3;
 herm Q^{3,3} c5 = a1^(-1);
 diag Q^{3,3} c6 = a2 * a2;
-square Q^{3,3} c7 = a1 + a2;
+square Q^{3,3} c7 = a1 + [0 1 2;3 4 5;6 7 8];
 //diag Q^{3,3} c8 = a2';
 psd Q^{3,3} c9 = a7 + a7;
 psd Q^{3,3} c10 = a1 + a7;
