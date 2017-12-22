a43ea16bead1576be35a46ec0801a0c59d9f528c
pichler <pichler@se-rwth.de>
2017-12-22 13:19:19 +0100

    First changes in Math Grammar

---
+++ script.groovy
@@ -0,0 +1,109 @@
+/*
+ * ******************************************************************************
+ * MontiCore Language Workbench, www.monticore.de
+ * Copyright (c) 2017, MontiCore, All rights reserved.
+ *
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * ******************************************************************************
+ */
+
+package de.monticore
+
+// M1: configuration object "_configuration" prepared externally
+Log.debug("--------------------------------", LOG_ID)
+Log.debug("MontiCore", LOG_ID)
+Log.debug(" - eating your models since 2005", LOG_ID)
+Log.debug("--------------------------------", LOG_ID)
+Log.debug("Grammar argument    : " + _configuration.getGrammarsAsStrings(), LOG_ID)
+Log.debug("Grammar files       : " + grammars, LOG_ID)
+Log.debug("Modelpath           : " + modelPath, LOG_ID)
+Log.debug("Output dir          : " + out, LOG_ID)
+Log.debug("Handcoded argument  : " + _configuration.getHandcodedPathAsStrings(), LOG_ID)
+Log.debug("Handcoded files     : " + handcodedPath, LOG_ID)
+
+// ############################################################
+// M1  basic setup and initialization:
+// initialize incremental generation; enabling of reporting; create global scope
+IncrementalChecker.initialize(out)
+InputOutputFilesReporter.resetModelToArtifactMap()
+globalScope = createGlobalScope(modelPath)
+Reporting.init(out.getAbsolutePath(), reportManagerFactory)
+// ############################################################
+
+// ############################################################
+// the first pass processes all input grammars up to transformation to CD and storage of the resulting CD to disk
+
+while (grammarIterator.hasNext()) {
+  input = grammarIterator.next()
+  if (force || !IncrementalChecker.isUpToDate(input, out, modelPath, templatePath, handcodedPath )) {
+    cleanUp(input)
+    
+    // M2: parse grammar
+    astGrammar = parseGrammar(input)
+    
+    if (astGrammar.isPresent()) {
+      astGrammar = astGrammar.get()
+      
+       // start reporting
+      grammarName = Names.getQualifiedName(astGrammar.getPackage(), astGrammar.getName())
+      Reporting.on(grammarName)
+	  Reporting.reportModelStart(astGrammar, grammarName, "")
+      Reporting.reportParseInputFile(input, grammarName)
+
+      // M3: populate symbol table
+      astGrammar = createSymbolsFromAST(globalScope, astGrammar)
+
+      // M4: execute context conditions
+      runGrammarCoCos(astGrammar, globalScope)
+
+      // M5: transform grammar AST into Class Diagram AST
+      astClassDiagram = transformAstGrammarToAstCd(glex, astGrammar, globalScope, handcodedPath)
+      astClassDiagramWithST = createSymbolsFromAST(globalScope, astClassDiagram)
+
+      // write Class Diagram AST to the CD-file (*.cd)
+      storeInCdFile(astClassDiagramWithST, out)
+
+      // M6: generate parser and wrapper
+      generateParser(glex, astGrammar, globalScope, handcodedPath, out)
+    }
+  }
+}
+// end of first pass
+// ############################################################
+
+// ############################################################
+// the second pass
+// do the rest which requires already created CDs of possibly
+// local super grammars etc.
+for (astGrammar in getParsedGrammars()) {
+  // make sure to use the right report manager again
+  Reporting.on(Names.getQualifiedName(astGrammar.getPackage(), astGrammar.getName()))
+  reportGrammarCd(astGrammar, globalScope, out)
+
+  astClassDiagram = getCDOfParsedGrammar(astGrammar)
+
+  // M7: decorate Class Diagram AST
+  decorateCd(glex, astClassDiagram, globalScope, handcodedPath)
+
+  // M8: generate symbol table
+  generateSymbolTable(astGrammar, globalScope, astClassDiagram, out, handcodedPath)
+  
+  // M9 Generate ast classes, visitor and context condition
+  generate(glex, globalScope, astClassDiagram, out, templatePath)
+
+  Log.info("Grammar " + astGrammar.getName() + " processed successfully!", LOG_ID)
+
+  // M10: flush reporting
+  Reporting.reportModelEnd(astGrammar.getName(), "")
+  Reporting.flush(astGrammar)
+}
---
+++ src/test/resources/Calculations/SteeringAngleCalculator.m
@@ -0,0 +1,88 @@
+/*
+ * ******************************************************************************
+ * MontiCore Language Workbench, www.monticore.de
+ * Copyright (c) 2017, MontiCore, All rights reserved.
+ *
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * ******************************************************************************
+ */
+
+package calculations;
+
+script SteeringAngleCalculator
+     Q(-90:90)^{2} x;
+     Q(-180:180)^{2} y;
+     Q(-90:90) gpsX;
+     Q(-180:180) gpsY;
+     Q(-180:180) orientation;
+     Q(-180:180) currentSteeringAngle;
+     Q(-180:180) minSteeringAngle;
+     Q(-180:180) maxSteeringAngle;
+     Q(-180:180) newSteeringAngle;
+     
+     Q distance;
+     Q res =(y(2) - y(1))*gpsX;
+     res -= (x(2) - x(1))*gpsY;
+     res += x(2) * y(1);
+     res -= y(2) * x(1);
+     Q xDiff = x(1) - x(2);
+     Q yDiff = y(1) - y(2);
+     res /= sqrt(xDiff*xDiff+yDiff*yDiff);
+
+     distance = res;
+
+     //calculate base steering angle
+     Q globalOrientation = orientation*(M_PI/180);
+     if (globalOrientation > M_PI){
+         globalOrientation -= 2 * M_PI;
+     }end
+
+     Q orientedDistance = distance;
+     Q angleTowardsTrajectory = atan(orientedDistance / 2);
+     Q orientationOfTrajectory;
+
+
+     Q v1 = x(2) - x(1);
+     Q v2 = y(2) - y(1);
+     Q cosineAngle = v2 / sqrt(v1 * v1 + v2 * v2);
+     Q angle = acos(cosineAngle);
+
+     if (v1 > 0){
+         orientationOfTrajectory = -angle;
+     }else{
+         orientationOfTrajectory = angle;
+     }end
+     Q angleTrajectoryAndCarDirection = orientationOfTrajectory - globalOrientation;
+
+     //the resulting angle is the angle needed to steer the car parallel to the trajectory
+     // plus the angle towards the trajectory
+     Q finalAngle = angleTrajectoryAndCarDirection + angleTowardsTrajectory;
+
+     //correct angle
+     if (finalAngle > M_PI){
+         finalAngle -= 2 * M_PI;
+     }elseif (finalAngle < -M_PI){
+          finalAngle += 2 * M_PI;
+     }end
+
+     newSteeringAngle = finalAngle*(180/M_PI);
+
+     //correct angle depending on car
+        angle = newSteeringAngle;
+        if angle < minSteeringAngle{
+            angle = minSteeringAngle;
+        }elseif angle > maxSteeringAngle{
+            angle = maxSteeringAngle;
+        }end
+        newSteeringAngle = -angle;
+end
