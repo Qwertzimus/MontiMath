988c3329c272217257e586d3f2e2f982711beebe
Christoph Richter <christoph.richter@rwth-aachen.de>
2018-05-18 12:28:43 +0200

    ASTTest: Adapted AST tests to new AST. Some nodes do not exist anymore.

--- src/test/java/de/monticore/lang/math/ASTTest.java
+++ src/test/java/de/monticore/lang/math/ASTTest.java
@@ -21,10 +21,12 @@
 package de.monticore.lang.math;
 
 import de.monticore.ast.ASTNode;
+import de.monticore.commonexpressions._ast.ASTPlusExpression;
 import de.monticore.lang.math._ast.*;
 import de.monticore.lang.math._parser.MathParser;
 import de.monticore.lang.math._visitor.MathVisitor;
-import de.monticore.lang.monticar.types2._ast.ASTElementType;
+import de.monticore.lang.matrix._ast.ASTMathMatrixValueExplicitExpression;
+import de.monticore.lang.monticar.commonexpressions._ast.ASTCommonAdditionExpression;
 import de.se_rwth.commons.Joiners;
 import org.jscience.mathematics.number.Rational;
 import org.junit.Test;
@@ -48,146 +50,56 @@ public class ASTTest {
     @Test
     public void testElementType() throws IOException {
         MathParser parser = new MathParser();
-        ASTElementType ast = parser.parseString_ElementType("Q").orElse(null);
+        ASTElementType ast = parser.parse_StringElementType("Q").orElse(null);
         assertNotNull(ast);
 
-        ast = parser.parseString_ElementType("Q(1 : 3 : 7)").orElse(null);
+        ast = parser.parse_StringElementType("Q(1 : 3 : 7)").orElse(null);
         assertNotNull(ast);
 
-        ast = parser.parseString_ElementType("Q(10 km : 20 km : 30 km)").orElse(null);
+        ast = parser.parse_StringElementType("Q(10 km : 20 km : 30 km)").orElse(null);
         assertNotNull(ast);
 
-        ast = parser.parseString_ElementType("Q(10 km : 20 km : 30 km)^{10, 15}").orElse(null);
+        ast = parser.parse_StringElementType("Q(10 km : 20 km : 30 km)^{10, 15}").orElse(null);
         assertNotNull(ast);
     }
 
     @Test
     public void testAssignment() throws IOException {
         MathParser parser = new MathParser();
-        ASTMathAssignmentDeclarationExpression ast = parser.parseString_MathAssignmentDeclarationExpression("Q^{2,2} A = [1 2; 3 4];").orElse(null);
+        ASTMathAssignmentDeclarationExpression ast = parser.parse_StringMathAssignmentDeclarationExpression("Q^{2,2} A = [1 2; 3 4];").orElse(null);
         assertNotNull(ast);
-    }
-
-    @Test
-    public void testArithmeticExpression() throws IOException {
-        MathParser parser = new MathParser();
-        ASTMathArithmeticExpression ast = parser.parseString_MathArithmeticExpression("1 m/s + 7 m/s").orElse(null);
-        assertNotNull(ast);
-
-        final List<ASTUnitNumber> numbers = new ArrayList<>();
-        MathVisitor visitor = new MathVisitor() {
-            @Override
-            public void visit(ASTUnitNumber node) {
-                numbers.add(node);
-            }
-        };
-        ast.accept(visitor);
-
-        assertEquals(2, numbers.size());
-
-        List<Integer> iList = numbers.stream().map(n -> n.getNumber().get().intValue())
-                .collect(Collectors.toList());
-        assertTrue(iList.containsAll(Arrays.asList(1, 7)));
-    }
-
-    @Test
-    public void testArithmeticExpression2() throws IOException {
-        MathParser parser = new MathParser();
-        ASTMathArithmeticExpression ast = parser.parseString_MathArithmeticExpression("1/2 + 3/2").orElse(null);
-        assertNotNull(ast);
-
-        final List<ASTUnitNumber> numbers = new ArrayList<>();
-        MathVisitor visitor = new MathVisitor() {
-            @Override
-            public void visit(ASTUnitNumber node) {
-                numbers.add(node);
-            }
-        };
-        ast.accept(visitor);
-
-        assertEquals(2, numbers.size());
-
-        List<Rational> iList = numbers.stream().map(n -> n.getNumber().get())
-                .collect(Collectors.toList());
-        assertTrue(iList.containsAll(Arrays.asList(Rational.valueOf(1, 2), Rational.valueOf(3, 2))));
-    }
-
-    @Test
-    public void testArithmeticExpression3() throws IOException {
-        MathParser parser = new MathParser();
-        ASTMathArithmeticExpression ast = parser.parseString_MathArithmeticExpression("2 - 3i * -4 + 3i").orElse(null);
-        assertNotNull(ast);
-
-        final List<ASTComplexNumber> numbers = new ArrayList<>();
-        final List<Boolean> mul = new ArrayList<>();
-        MathVisitor visitor = new MathVisitor() {
-            @Override
-            public void visit(ASTComplexNumber node) {
-                numbers.add(node);
-            }
-
-            public void visit(ASTMathArithmeticMultiplicationExpression node) {
-                mul.add(true);
-            }
-
-            public void visit(ASTMathArithmeticMatrixMultiplicationExpression node) {
-                mul.add(true);
-            }
-
-        };
-        ast.accept(visitor);
-
-        assertEquals(2, numbers.size());
-        assertEquals(1, mul.size()); // so that we are sure to have exactly one infix * AST
-
-        assertEquals(Rational.valueOf(2, 1), numbers.get(0).getReal());
-        assertEquals(Rational.valueOf(-3, 1), numbers.get(0).getImg());
-
-        assertEquals(Rational.valueOf(-4, 1), numbers.get(1).getReal());
-        assertEquals(Rational.valueOf(3, 1), numbers.get(1).getImg());
     }
 
     @Test
     public void testArithmeticExpression4() throws IOException {
         MathParser parser = new MathParser();
-        ASTMathAssignmentExpression ast = parser.parseString_MathAssignmentExpression("v = (1+m/s);").orElse(null);
+        ASTMathAssignmentExpression ast = parser.parse_StringMathAssignmentExpression("v = (1+m/s);").orElse(null);
         assertNotNull(ast);
 
         final List<ASTNode> numbers = new ArrayList<>();
         final List<Boolean> div = new ArrayList<>();
         final List<String> names = new ArrayList<>();
         MathVisitor visitor = new MathVisitor() {
-            @Override
+
             public void visit(ASTComplexNumber node) {
                 numbers.add(node);
             }
 
-            @Override
             public void visit(ASTUnitNumber node) {
                 numbers.add(node);
             }
 
-            public void visit(ASTMathArithmeticDivisionExpression node) {
-                div.add(true);
-            }
-
-            public void visit(ASTMathNameExpression node) {
-
-                names.add(node.getName());
-
-            }
-
             public void visit(ASTMathMatrixNameExpression node) {
-                names.add(node.getName().get());
+                names.add(node.getName());
             }
 
             public void visit(ASTMathAssignmentExpression node) {
-                if (node.getName().isPresent())
-                    names.add(node.getName().get());
-                else if(node.getDottedName().isPresent())
-                    names.add(Joiners.DOT.join(node.getDottedName().get().getNames()));
-                else if(node.getMathMatrixNameExpression().isPresent())
-                    names.add(node.getMathMatrixNameExpression().get().getName().get());//TOdo add access info?
+                if (node.getNameOpt().isPresent())
+                    names.add(node.getNameOpt().get());
+                else if(node.getMathDottedNameExpressionOpt().isPresent())
+                    names.add(Joiners.DOT.join(node.getMathDottedNameExpressionOpt().get().getNameList()));
+                else if(node.getMathMatrixNameExpressionOpt().isPresent())
+                    names.add(node.getMathMatrixNameExpressionOpt().get().getName());//TOdo add access info?
             }
 
         };
@@ -203,7 +115,7 @@ public class ASTTest {
     @Test
     public void testMatrix() throws IOException {
         MathParser parser = new MathParser();
-        ASTMathMatrixValueExplicitExpression ast = parser.parseString_MathMatrixValueExplicitExpression("[1 2; 3 4]").orElse(null);
+        ASTMathMatrixValueExplicitExpression ast = parser.parse_StringMathMatrixValueExplicitExpression("[1 2; 3 4]").orElse(null);
         assertNotNull(ast);
 
     }
